944b1d873c6a10237a8d9fde3f2db8867b407b2e
Carmine <neos7@live.it>
2019-05-16 13:03:53 +0200

    added SetupTrailMode

--- LightTrail/Script.cs
+++ LightTrail/Script.cs
@@ -27,9 +27,6 @@ namespace LightTrail
         #region Private Members
 
         private TrailMode trailMode = TrailMode.BrakeOnly;
-        private string boneL;
-        private string boneR;
-        private string boneM;
         private int playerVehicle;
         private TrailFx trailL = new TrailFx("left");
         private TrailFx trailR = new TrailFx("right");
@@ -42,13 +39,14 @@ namespace LightTrail
             Off,
             On,
             BrakeOnly,
-
         }
 
         class TrailFx
         {
             public string Name { get; set; }
             public int Handle { get; set; }
+            public string BoneName { get; set; }
+            public bool Enabled { get; set; } = true;
             public Vector3 Offset { get; set; } = Vector3.Zero;
             public Vector3 Rotation { get; set; } = Vector3.Zero;
             public Vector3 Color { get; set; } = new Vector3(1.0f, 0.0f, 0.0f);
@@ -67,20 +65,22 @@ namespace LightTrail
             {
                 if (args.Count < 1)
                 {
-                    Debug.WriteLine($"LightTrail: Missing argument off|on|brakeonly");
+                    Debug.WriteLine($"LightTrail: Missing argument off|on|brake");
                     return;
                 }
 
-                switch(args[0])
+                switch (args[0])
                 {
-                    case "on":
-                        trailMode = TrailMode.On;
-                        break;
                     case "off":
                         trailMode = TrailMode.Off;
                         break;
+                    case "on":
+                        trailMode = TrailMode.On;
+                        SetupTrailMode();
+                        break;
                     case "brake":
                         trailMode = TrailMode.BrakeOnly;
+                        SetupTrailMode();
                         break;
                     default:
                         Debug.WriteLine($"LightTrail: Error parsing {args[0]}");
@@ -105,8 +105,30 @@ namespace LightTrail
                 await Delay(0);
             }
 
+            await SetupTrailMode();
+
             Tick += GetPlayerVehicle;
             Tick += UpdatePlayerVehicle;
+        }
+
+        private async Task SetupTrailMode()
+        {
+            switch (trailMode)
+            {
+                case TrailMode.Off:
+                    break;
+                case TrailMode.On:
+                    trailL.BoneName = taillight_l; // GetEntityBoneIndexByName(playerVehicle, taillight_l) != -1 ? taillight_l : brakelight_l;
+                    trailR.BoneName = taillight_r; // GetEntityBoneIndexByName(playerVehicle, taillight_r) != -1 ? taillight_r : brakelight_r;
+                    trailM.BoneName = taillight_m; // GetEntityBoneIndexByName(playerVehicle, taillight_m) != -1 ? taillight_m : brakelight_m;
+                    break;
+                case TrailMode.BrakeOnly:
+                    trailL.BoneName = GetEntityBoneIndexByName(playerVehicle, brakelight_l) != -1 ? brakelight_l : taillight_l;
+                    trailR.BoneName = GetEntityBoneIndexByName(playerVehicle, brakelight_r) != -1 ? brakelight_r : taillight_r;
+                    trailM.BoneName = GetEntityBoneIndexByName(playerVehicle, brakelight_m) != -1 ? brakelight_m : taillight_m;
+                    break;
+            }
+            await Task.FromResult(0);
         }
 
         private async Task UpdatePlayerVehicle()
@@ -116,6 +138,10 @@ namespace LightTrail
 
             await UpdateVehiclePtfx(playerVehicle);
         }
+
+        private bool PlayerIsBraking => (GetEntitySpeed(playerVehicle) != 0.0f && GetEntitySpeedVector(playerVehicle, true).Y > 0.0f && 
+            (IsControlPressed(1, (int)Control.VehicleBrake) || IsDisabledControlPressed(1, (int)Control.VehicleBrake) || 
+            IsControlJustPressed(1, (int)Control.VehicleBrake) || IsDisabledControlJustPressed(1, (int)Control.VehicleBrake)));
 
         private async Task UpdateVehiclePtfx(int entity)
         {
@@ -127,9 +153,7 @@ namespace LightTrail
                     
                 case TrailMode.On:
                     UseParticleFxAssetNextCall(dict);
-                    boneL = taillight_l;
-                    boneR = taillight_r;
-                    boneM = taillight_m;
+                    
                     StartForTrail(trailL);
                     StartForTrail(trailR);
                     StartForTrail(trailM);
@@ -137,11 +161,7 @@ namespace LightTrail
 
                 case TrailMode.BrakeOnly:
 
-                    boneL = GetEntityBoneIndexByName(entity, brakelight_l) != -1 ? brakelight_l : taillight_l;
-                    boneR = GetEntityBoneIndexByName(entity, brakelight_r) != -1 ? brakelight_r : taillight_r;
-                    boneM = GetEntityBoneIndexByName(entity, brakelight_m) != -1 ? brakelight_m : taillight_m;
-
-                    if (GetEntitySpeed(playerVehicle) != 0.0f && GetEntitySpeedVector(playerVehicle, true).Y > 0.0f && (IsControlPressed(1, (int)Control.VehicleBrake) || IsDisabledControlPressed(1, (int)Control.VehicleBrake) || IsControlJustPressed(1, (int)Control.VehicleBrake) || IsDisabledControlJustPressed(1, (int)Control.VehicleBrake)))
+                    if (PlayerIsBraking)
                     {
                         //Debug.WriteLine($"Speed: {GetEntitySpeed(playerVehicle)}, Vector: {GetEntitySpeedVector(playerVehicle, true)}, Brake: {(IsControlPressed(1, (int)Control.VehicleBrake) || IsDisabledControlPressed(1, (int)Control.VehicleBrake) || IsControlJustPressed(1, (int)Control.VehicleBrake) || IsDisabledControlJustPressed(1, (int)Control.VehicleBrake))}");
 
@@ -167,13 +187,13 @@ namespace LightTrail
                     switch (trail.Name)
                     {
                         case "left":
-                            StartParticleFx(ref handle, particleName, entity, boneL, trail.Color, trail.Offset, trail.Rotation, trail.Scale, trail.Alpha);
+                            StartParticleFx(ref handle, particleName, entity, trail.BoneName, trail.Color, trail.Offset, trail.Rotation, trail.Scale, trail.Alpha);
                             break;
                         case "middle":
-                            StartParticleFx(ref handle, particleName, entity, boneM, trail.Color, trail.Offset, trail.Rotation, trail.Scale, trail.Alpha);
+                            StartParticleFx(ref handle, particleName, entity, trail.BoneName, trail.Color, trail.Offset, trail.Rotation, trail.Scale, trail.Alpha);
                             break;
                         case "right":
-                            StartParticleFx(ref handle, particleName, entity, boneR, trail.Color, trail.Offset, trail.Rotation, trail.Scale, trail.Alpha);
+                            StartParticleFx(ref handle, particleName, entity, trail.BoneName, trail.Color, trail.Offset, trail.Rotation, trail.Scale, trail.Alpha);
                             break;
                     }
 
